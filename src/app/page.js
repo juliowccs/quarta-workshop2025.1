2c9d6aebc26a1b1879d5ed622a906180ba4e0d13
Júlio César <julioceasarcs@gmail.com>
2025-02-18 18:00:45 -0300

    :sparkles: feat: Add headings to character pages and character list

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import CharacterList from '@/components/CharacterList/CharacterList'
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start px-24 py-24 bg-slate-800">
+    <main className="flex min-h-screen flex-col items-center justify-start px-24 py-12 bg-slate-800">
       <CharacterList />
     </main>
   )
--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -20,6 +20,7 @@ export default function CharacterList() {
 
   return (
     <>
+      <h1 className='font-bold text-4xl mb-8'>Rick And Morty Characters</h1>
       <input
         type="text"
         onChange={(e) => setSearch(e.target.value)}
