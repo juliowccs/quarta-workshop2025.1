382936d14bfeacda15ef6a2d8b5b838e3e7868af
Júlio César Carvalho Santos <julioceasarcs@gmail.com>
2025-02-26 12:29:03 -0300

    :wrencht: fix: Card instead of Cart on component

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import React, { useEffect, useState } from 'react'
-import CharacterCard from '../CharacterCart/CharacterCart'
+import CharacterCard from '../CharacterCard/CharacterCard'
 
 export default function CharacterList() {
   const [data, setData] = useState([])
